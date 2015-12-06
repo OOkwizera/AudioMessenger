28cd58f2ace315ffb37195175280e6017bf2cdee
OOkwizera <kwizoliv010@gmail.com>
2015-12-06 17:32:56 -0600

    Original audiocapture with the working audioplay

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	    public void start(Stage primaryStage) throws Exception{
 	        TabPane root = FXMLLoader.load(getClass().getResource("Gui.fxml"));
 	        primaryStage.setTitle("AudioMessenger");
-	        primaryStage.setScene(new Scene(root, 600, 400));
+	        primaryStage.setScene(new Scene(root, 300, 400));
 	        primaryStage.show();
 
 	    }
